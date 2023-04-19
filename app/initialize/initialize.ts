4101dac7d04b648dca1d389780488c16e07f1f64
NghiaLMT <leminh@tng-soft.com.vn>
2023-04-19 15:38:32 +0700

    :hearts: config notification

--- app/initialize/initialize.ts
+++ app/initialize/initialize.ts
@@ -1,7 +1,6 @@
 import {logInfo} from '@app/utils';
 import LocalDatabase from '@database/local_database';
 import FCMService from '@managers/fcm_service';
-import FirebaseConfig from '@managers/firebase_config';
 import GlobalEventHandler from '@managers/global_event_handler';
 
 let alreadyInitialized = false;
@@ -9,16 +8,13 @@ let alreadyInitialized = false;
 export const initialize = async () => {
     if (!alreadyInitialized) {
         alreadyInitialized = true;
-        Promise.all([
-            GlobalEventHandler.init(),
-            LocalDatabase.init(),
-            FCMService.init(),
-            FirebaseConfig.init(),
-        ]).then(values => {
-            const result = values.filter(x => !x);
-            if (result.length === 0) {
-                logInfo('[LAUNCHER] Successful initialization !!!');
+        Promise.all([GlobalEventHandler.init(), LocalDatabase.init(), FCMService.init()]).then(
+            values => {
+                const result = values.filter(x => !x);
+                if (result.length === 0) {
+                    logInfo('[LAUNCHER] Successful initialization !!!');
+                }
             }
-        });
+        );
     }
 };
