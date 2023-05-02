e34d856868291bbf45fcd43570d98731d0b69801
NghiaLMT <leminh@tng-soft.com.vn>
2023-05-02 22:03:49 +0700

    Keep calm and commit

---
+++ test/setup.js
@@ -0,0 +1,57 @@
+/* eslint-disable react/no-multi-comp */
+
+import * as ReactNative from 'react-native';
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+
+jest.doMock('react-native', () => {
+    const {
+        Platform,
+        StyleSheet,
+        requireNativeComponent,
+        Alert: RNAlert,
+        InteractionManager: RNInteractionManager,
+        NativeModules: RNNativeModules,
+        Linking: RNLinking,
+    } = ReactNative;
+
+    const Alert = {
+        ...RNAlert,
+        alert: jest.fn(),
+    };
+
+    const InteractionManager = {
+        ...RNInteractionManager,
+        runAfterInteractions: jest.fn(cb => cb()),
+    };
+
+    const Linking = {
+        ...RNLinking,
+        openURL: jest.fn(),
+        addEventListener: jest.fn(() => {
+            return {remove: jest.fn()};
+        }),
+    };
+
+    return Object.setPrototypeOf(
+        {
+            Platform: {
+                ...Platform,
+                OS: 'ios',
+                Version: 12,
+                constants: {
+                    reactNativeVersion: {
+                        major: 0,
+                        minor: 64,
+                    },
+                },
+            },
+            StyleSheet,
+            requireNativeComponent,
+            Alert,
+            InteractionManager,
+            Linking,
+        },
+        ReactNative
+    );
+});
+jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
